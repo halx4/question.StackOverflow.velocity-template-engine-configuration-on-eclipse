21342798be3d429d3a72fbd3804ad39403fcb78d
halx4 <halx4@users.noreply.github.com>
2016-04-25 16:00:48 +0300

    initial commit

---
+++ NoMavenClient/.apt_generated/deors/demos/annotations/velocity/client/ArticleBeanInfo.java
@@ -0,0 +1,365 @@
+package deors.demos.annotations.velocity.client;
+//V2
+import java.beans.BeanDescriptor;
+import java.beans.IntrospectionException;
+import java.beans.MethodDescriptor;
+import java.beans.ParameterDescriptor;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+public class ArticleBeanInfo extends java.beans.SimpleBeanInfo {
+
+    /**
+     * Gets the bean class object.
+     *
+     * @return the bean class
+     */
+    public static Class<?> getBeanClass() {
+
+        return deors.demos.annotations.velocity.client.Article.class;
+    }
+
+    /**
+     * Gets the bean class name.
+     *
+     * @return the bean class name
+     */
+    public static String getBeanClassName() {
+
+        return "deors.demos.annotations.velocity.client.Article";
+    }
+
+    /**
+     * Returns the deors.demos.annotations.velocity.client.Article bean descriptor.
+     *
+     * @return the bean descriptor
+     */
+    public BeanDescriptor getBeanDescriptor() {
+
+        BeanDescriptor descriptor = new BeanDescriptor(getBeanClass());
+
+        descriptor.setName("Article");
+        descriptor.setDisplayName("Article");
+        descriptor.setShortDescription("This JavaBean represents an Article in the On-line Store application");
+        descriptor.setExpert(false);
+        descriptor.setHidden(false);
+        descriptor.setPreferred(false);
+
+        return descriptor;
+    }
+
+    /**
+     * Returns the list of property descriptors for the deors.demos.annotations.velocity.client.Article bean.
+     *
+     * @return the list of property descriptors
+     */
+    public PropertyDescriptor[] getPropertyDescriptors() {
+
+        return new PropertyDescriptor[] {
+            getIdPropertyDescriptor()
+            getDepartmentPropertyDescriptor()
+            getStatusPropertyDescriptor()
+        };
+    }
+
+    /**
+     * Returns the list of method descriptors for the deors.demos.annotations.velocity.client.Article bean.
+     *
+     * @return the list of method descriptors
+     */
+    public MethodDescriptor[] getMethodDescriptors() {
+
+        return new MethodDescriptor[] {
+            getIsInactiveMethodDescriptor()
+            getActivateMethodDescriptor()
+            getInvalidateMethodDescriptor()
+            getIsInvalidMethodDescriptor()
+            getIsActiveMethodDescriptor()
+            getDeactivateMethodDescriptor()
+        };
+    }
+
+    /**
+     * Returns the id property descriptor.
+     *
+     * @return the property descriptor
+     */
+    public PropertyDescriptor getIdPropertyDescriptor() {
+
+        // create a property descriptor with usual names for getter and setter
+        try {
+            PropertyDescriptor descriptor = new PropertyDescriptor("id", getBeanClass());
+
+            descriptor.setBound(true);
+            descriptor.setName("id");
+            descriptor.setDisplayName("id");
+            descriptor.setShortDescription("The unique identifier for this Article in the catalog");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (IntrospectionException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the department property descriptor.
+     *
+     * @return the property descriptor
+     */
+    public PropertyDescriptor getDepartmentPropertyDescriptor() {
+
+        // create a property descriptor with usual names for getter and setter
+        try {
+            PropertyDescriptor descriptor = new PropertyDescriptor("department", getBeanClass());
+
+            descriptor.setBound(true);
+            descriptor.setName("department");
+            descriptor.setDisplayName("department");
+            descriptor.setShortDescription("The department number where this Article belongs to");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (IntrospectionException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the status property descriptor.
+     *
+     * @return the property descriptor
+     */
+    public PropertyDescriptor getStatusPropertyDescriptor() {
+
+        // create a property descriptor with usual names for getter and setter
+        try {
+            PropertyDescriptor descriptor = new PropertyDescriptor("status", getBeanClass());
+
+            descriptor.setBound(true);
+            descriptor.setName("status");
+            descriptor.setDisplayName("status");
+            descriptor.setShortDescription("Textual representation of this Article status");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (IntrospectionException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the isInactive() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getIsInactiveMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {};
+            Method method = getBeanClass().getMethod("isInactive", parameterTypes);
+
+            // creates the method parameter descriptors
+
+            ParameterDescriptor[] parameterDescriptors = {
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("isInactive()");
+            descriptor.setDisplayName("isInactive()");
+            descriptor.setShortDescription("");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the activate() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getActivateMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {};
+            Method method = getBeanClass().getMethod("activate", parameterTypes);
+
+            // creates the method parameter descriptors
+
+            ParameterDescriptor[] parameterDescriptors = {
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("activate()");
+            descriptor.setDisplayName("activate()");
+            descriptor.setShortDescription("");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the invalidate() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getInvalidateMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {java.lang.String.class};
+            Method method = getBeanClass().getMethod("invalidate", parameterTypes);
+
+            // creates the method parameter descriptors
+            ParameterDescriptor parameterDescriptor1 = new ParameterDescriptor();
+            parameterDescriptor1.setName("cause");
+            parameterDescriptor1.setDisplayName("cause");
+            parameterDescriptor1.setShortDescription("The cause why the Article is invalidated");
+            parameterDescriptor1.setExpert(false);
+            parameterDescriptor1.setHidden(false);
+            parameterDescriptor1.setPreferred(false);
+
+            ParameterDescriptor[] parameterDescriptors = {
+                parameterDescriptor1
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("invalidate(java.lang.String)");
+            descriptor.setDisplayName("invalidate(java.lang.String)");
+            descriptor.setShortDescription("Action to invalidate the Article, given the cause");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the isInvalid() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getIsInvalidMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {};
+            Method method = getBeanClass().getMethod("isInvalid", parameterTypes);
+
+            // creates the method parameter descriptors
+
+            ParameterDescriptor[] parameterDescriptors = {
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("isInvalid()");
+            descriptor.setDisplayName("isInvalid()");
+            descriptor.setShortDescription("");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the isActive() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getIsActiveMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {};
+            Method method = getBeanClass().getMethod("isActive", parameterTypes);
+
+            // creates the method parameter descriptors
+
+            ParameterDescriptor[] parameterDescriptors = {
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("isActive()");
+            descriptor.setDisplayName("isActive()");
+            descriptor.setShortDescription("");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the deactivate() method descriptor.
+     *
+     * @return the method descriptor
+     */
+    public MethodDescriptor getDeactivateMethodDescriptor() {
+
+        try {
+            // finds the method using getMethod with parameter types
+            Class<?>[] parameterTypes = {};
+            Method method = getBeanClass().getMethod("deactivate", parameterTypes);
+
+            // creates the method parameter descriptors
+
+            ParameterDescriptor[] parameterDescriptors = {
+            };
+
+            MethodDescriptor descriptor = new MethodDescriptor(method, parameterDescriptors);
+
+            descriptor.setName("deactivate()");
+            descriptor.setDisplayName("deactivate()");
+            descriptor.setShortDescription("");
+            descriptor.setExpert(false);
+            descriptor.setHidden(false);
+            descriptor.setPreferred(false);
+
+            return descriptor;
+
+        } catch (NoSuchMethodException | SecurityException e) {
+            return null;
+        }
+    }
+}
---
+++ NoMavenProcessor/src/deors/demos/annotations/velocity/processors/BeanInfoProcessor.java
@@ -0,0 +1,237 @@
+package deors.demos.annotations.velocity.processors;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.ParseErrorException;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.apache.velocity.tools.generic.DisplayTool;
+import org.kohsuke.MetaInfServices;
+
+import deors.demos.annotations.velocity.BeanInfo;
+
+/**
+ * Annotation processor for BeanInfo annotation type. It generates a full featured
+ * BeanInfo type with the help of an Apache Velocity template.
+ *
+ * @author deors
+ * @version 1.0
+ */
+@SupportedAnnotationTypes("deors.demos.annotations.velocity.BeanInfo")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@MetaInfServices(Processor.class) 
+public class BeanInfoProcessor
+    extends AbstractProcessor {
+
+	public static final String LOGGER_NAME="root";
+	
+    /** String used to append to a class name when creating the BeanInfo class name. */
+    private static final String BEAN_INFO = "BeanInfo";
+
+    /**
+     * Default constructor.
+     */
+    public BeanInfoProcessor() {
+
+        super();
+    }
+
+    /**
+     * Reads the BeanInfo information and writes a full featured
+     * BeanInfo type with the help of an Apache Velocity template.
+     *
+     * @param annotations set of annotations found
+     * @param roundEnv the environment for this processor round
+     *
+     * @return whether a new processor round would be needed
+     */
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        if (annotations.isEmpty()) {
+            return true;
+        }
+
+        try {
+            BeanInfoTypeModel model = null;
+            Map<String, BeanInfoPropertyModel> properties = new HashMap<>();
+            Map<String, BeanInfoMethodModel> methods = new HashMap<>();
+
+            for (Element e : roundEnv.getElementsAnnotatedWith(BeanInfo.class)) {
+
+                if (e.getKind() == ElementKind.CLASS) {
+
+                    model = new BeanInfoTypeModel();
+
+                    TypeElement classElement = (TypeElement) e;
+                    PackageElement packageElement = (PackageElement) classElement.getEnclosingElement();
+                    BeanInfo annotation = classElement.getAnnotation(BeanInfo.class);
+
+                    model.packageName = packageElement.getQualifiedName().toString();
+                    model.className = classElement.getSimpleName().toString();
+                    model.qualifiedName = classElement.getQualifiedName().toString();
+                    model.beanInfoClassName = model.className + BEAN_INFO;
+                    model.beanInfoQualifiedName = model.qualifiedName + BEAN_INFO;
+                    model.description = annotation.description();
+
+                    processingEnv.getMessager().printMessage(
+                        Diagnostic.Kind.NOTE,
+                        "annotated class: " + model.qualifiedName, e);
+
+                } else if (e.getKind() == ElementKind.FIELD) {
+
+                    VariableElement varElement = (VariableElement) e;
+
+                    BeanInfoPropertyModel property = new BeanInfoPropertyModel();
+                    BeanInfo annotation = varElement.getAnnotation(BeanInfo.class);
+
+                    property.name = varElement.getSimpleName().toString();
+                    property.qualifiedType = varElement.asType().toString();
+                    property.description = annotation.description();
+                    property.expert = annotation.expert();
+                    property.hidden = annotation.hidden();
+                    property.preferred = annotation.preferred();
+
+                    properties.put(property.name, property);
+
+                    processingEnv.getMessager().printMessage(
+                        Diagnostic.Kind.NOTE,
+                        "annotated field: " + property.name + " // field type: " + property.qualifiedType, e);
+
+                } else if (e.getKind() == ElementKind.METHOD) {
+
+                    ExecutableElement exeElement = (ExecutableElement) e;
+                    List<? extends VariableElement> paramElements = exeElement.getParameters();
+                    BeanInfo annotation = exeElement.getAnnotation(BeanInfo.class);
+
+                    BeanInfoMethodModel method = new BeanInfoMethodModel();
+                    List<BeanInfoPropertyModel> parameters = new ArrayList<>();
+
+                    method.name = exeElement.getSimpleName().toString();
+                    method.qualifiedType = exeElement.getReturnType().toString();
+                    method.description = annotation.description();
+                    method.expert = annotation.expert();
+                    method.hidden = annotation.hidden();
+                    method.preferred = annotation.preferred();
+                    method.parameters = parameters;
+
+                    for (VariableElement paramElement : paramElements) {
+                        BeanInfoPropertyModel parameter = new BeanInfoPropertyModel();
+                        BeanInfo paramAnnotation = paramElement.getAnnotation(BeanInfo.class);
+
+                        parameter.name = paramElement.getSimpleName().toString();
+                        parameter.qualifiedType = paramElement.asType().toString();
+                        parameter.description = paramAnnotation.description();
+                        parameter.expert = paramAnnotation.expert();
+                        parameter.hidden = paramAnnotation.hidden();
+                        parameter.preferred = paramAnnotation.preferred();
+
+                        parameters.add(parameter);
+                    }
+
+                    methods.put(method.name, method);
+
+                    processingEnv.getMessager().printMessage(
+                        Diagnostic.Kind.NOTE,
+                        "annotated method: " + method.name + " // return type: " + method.qualifiedType, e);
+                    for (BeanInfoPropertyModel parameter : parameters) {
+                        processingEnv.getMessager().printMessage(
+                            Diagnostic.Kind.NOTE,
+                            "parameter: " + parameter.name + " // parameter type: " + parameter.qualifiedType, e);
+                    }
+                }
+            }
+
+            if (model != null) {
+            	printNote("MARK1");
+
+                Properties props = new Properties();
+                URL url = this.getClass().getClassLoader().getResource("velocity.properties");
+                props.load(url.openStream());
+                props.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogSystem");
+                printNote("MARK2");
+                VelocityEngine ve = new VelocityEngine();
+                printNote("MARK3");
+               try{ ve.init(props); }
+               catch(Exception e){printNote("FAIL");}
+                
+                printNote("MARK4");
+                VelocityContext vc = new VelocityContext();
+                printNote("MARK5");
+                vc.put("model", model);
+                vc.put("properties", properties);
+                vc.put("methods", methods);
+
+                // adding DisplayTool from Velocity Tools library
+                vc.put("display", new DisplayTool());
+
+                Template vt = ve.getTemplate("beaninfo.vm");
+
+                JavaFileObject jfo = processingEnv.getFiler().createSourceFile(
+                    model.beanInfoQualifiedName);
+
+                processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.NOTE,
+                    "creating source file: " + jfo.toUri());
+
+                Writer writer = jfo.openWriter();
+
+                processingEnv.getMessager().printMessage(
+                    Diagnostic.Kind.NOTE,
+                    "applying velocity template: " + vt.getName());
+
+                vt.merge(vc, writer);
+
+                writer.close();
+            }
+        } catch (ResourceNotFoundException rnfe) {
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.ERROR,
+                rnfe.getLocalizedMessage());
+        } catch (ParseErrorException pee) {
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.ERROR,
+                pee.getLocalizedMessage());
+        } catch (IOException ioe) {
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.ERROR,
+                ioe.getLocalizedMessage());
+        } catch (Exception e) {
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.ERROR,
+                e.getLocalizedMessage());
+        }
+
+        return true;
+    }
+    
+    private void printNote(String message){
+    	processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE,message);
+    }
+    
+    public static void main(String[] args){};
+}
